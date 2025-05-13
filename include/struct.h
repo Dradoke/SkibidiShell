b7ea46f8abc1dbdf803f96b96f5f083fd27f85b3
Nathan.Gdn <74358790+Dradoke@users.noreply.github.com>
2025-05-13 17:04:12 +0200

    T_redir (#7)
    Co-authored-by: Nathan Gaudouin <ngaudoui@c2r11p2.42lehavre.fr>
    Co-authored-by: Nathan Gaudouin <ngaudoui@c1r2p4.42lehavre.fr>

---
+++ include/struct.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   struct.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: SkibidiShell - ngaudoui & mavander         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/21 21:42:42 by SkibidiShell      #+#    #+#             */
+/*   Updated: 2024/12/21 21:42:42 by SkibidiShell     ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRUCT_H
+# define STRUCT_H
+
+//---ENUMS---//
+typedef enum e_redir_type
+{
+	INFILE,		// <
+	OUTFILE,	// >
+	HEREDOC,	// << 
+	APPEND,		// >>
+}	t_redir_type;
+
+//---STRUCTS---//
+typedef struct s_redir
+{
+	unsigned char	name;
+	int				fd;
+	t_redir_type	type;
+}	t_redir;
+
+#endif
