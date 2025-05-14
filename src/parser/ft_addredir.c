bc570bd741952d585a1bbb906704f9580bad21cd
MV42 <149727332+MV42@users.noreply.github.com>
2025-05-14 12:54:21 +0200

    Added ft_addredir, i'm working on it (#11)
    * Added ft_addredir, i'm working on it
    
    * T_redir (#7)
    
    Co-authored-by: Nathan Gaudouin <ngaudoui@c2r11p2.42lehavre.fr>
    Co-authored-by: Nathan Gaudouin <ngaudoui@c1r2p4.42lehavre.fr>
    
    * enum for last_redir added in struct.h (#9)
    
    Co-authored-by: Nathan Gaudouin <ngaudoui@c1r2p4.42lehavre.fr>
    
    * Feat/t arg (#8)
    
    Co-authored-by: Nathan Gaudouin <ngaudoui@c1r2p4.42lehavre.fr>
    Co-authored-by: MV42 <149727332+MV42@users.noreply.github.com>
    
    * Feat/ft isdelim (#10)
    
    Co-authored-by: Nathan Gaudouin <ngaudoui@c1r2p4.42lehavre.fr>
    
    * Added skibidi_shell.h and prototype.h and deleted the gitkeep where not needed anymore
    
    * Finished getredirtype
    
    * I've added fterr.h for the error messages
    and i've finished getredirype
    need to see about ft_error function
    
    ---------
    
    Co-authored-by: Nathan.Gdn <74358790+Dradoke@users.noreply.github.com>
    Co-authored-by: Nathan Gaudouin <ngaudoui@c2r11p2.42lehavre.fr>
    Co-authored-by: Nathan Gaudouin <ngaudoui@c1r2p4.42lehavre.fr>

---
+++ src/parser/ft_addredir.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_addredir.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: SkibidiShell - ngaudoui & mavander         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/21 21:42:42 by SkibidiShell      #+#    #+#             */
+/*   Updated: 2024/12/21 21:42:42 by SkibidiShell     ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "skibidi_shell.h"
+
+t_redir_type get_redir_type(char *line, int *i)
+{
+	t_redir_type	type;
+
+	if (line[*i] == '<')
+	{
+		(*i)++;
+		if (line[*i] == '<')
+			type = HEREDOC;
+		else if (isprint(line[*i]) && !isdelim(line[*i]))
+			type = INFILE;
+		else
+			ft_error(FTERR_REDIR);
+	}
+	else if (line[*i] == '>')
+	{
+		(*i)++;
+		if (line[*i] == '>')
+			type == APPEND;
+		else if (isprint(line[*i]) && !isdelim(line[*i]))
+			type = OUTFILE;
+		else
+			ft_error(FTERR_REDIR);
+	}
+	(*i)++;
+	return (type);
+}
