a906965daf91581778c7510f006901415ef4cd73
MV42 <149727332+MV42@users.noreply.github.com>
2025-05-14 12:54:21 +0200

    Added ft_addredir, i'm working on it (#11)
    * Added ft_addredir, i'm working on it
    
    * T_redir (#7)
    
    Co-authored-by: Nathan Gaudouin <ngaudoui@c2r11p2.42lehavre.fr>
    Co-authored-by: Nathan Gaudouin <ngaudoui@c1r2p4.42lehavre.fr>
    
    * enum for last_redir added in struct.h (#9)
    
    Co-authored-by: Nathan Gaudouin <ngaudoui@c1r2p4.42lehavre.fr>
    
    * Feat/t arg (#8)
    
    Co-authored-by: Nathan Gaudouin <ngaudoui@c1r2p4.42lehavre.fr>
    Co-authored-by: MV42 <149727332+MV42@users.noreply.github.com>
    
    * Feat/ft isdelim (#10)
    
    Co-authored-by: Nathan Gaudouin <ngaudoui@c1r2p4.42lehavre.fr>
    
    * Added skibidi_shell.h and prototype.h and deleted the gitkeep where not needed anymore
    
    * Finished getredirtype
    
    * I've added fterr.h for the error messages
    and i've finished getredirype
    need to see about ft_error function
    
    ---------
    
    Co-authored-by: Nathan.Gdn <74358790+Dradoke@users.noreply.github.com>
    Co-authored-by: Nathan Gaudouin <ngaudoui@c2r11p2.42lehavre.fr>
    Co-authored-by: Nathan Gaudouin <ngaudoui@c1r2p4.42lehavre.fr>

---
+++ include/skibidi_shell.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   skibidi_shell.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: SkibidiShell - ngaudoui & mavander         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/21 21:42:42 by SkibidiShell      #+#    #+#             */
+/*   Updated: 2024/12/21 21:42:42 by SkibidiShell     ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SKIBIDI_SHELL_H
+# define SKIBIDI_SHELL_H
+
+# include "struct.h"
+# include "prototype.h"
+# include "fterr.h"
+
+#endif
