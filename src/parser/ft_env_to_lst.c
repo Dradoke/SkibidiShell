f61eb630ef537b5658d3ea959bdbdde0f9be48f2
Nathan Gaudouin <ngaudoui@c2r12p2.42lehavre.fr>
2025-05-16 21:25:08 +0200

    Function work, need to know what to use if an error occured

--- src/parser/ft_env_to_lst.c
+++ src/parser/ft_env_to_lst.c
@@ -14,69 +14,51 @@
 
 typedef struct s_env
 {
-	struct s_env	*prev;
-	int				index;
 	char			*name;
-	struct s_env	*next;
 }	t_env;
 
-t_env	*ft_env_to_lst(char **envp)
+
+t_list	*ft_env_to_lst(char **envp)
 {
-	t_env	*env_lst;
-	t_env	*head;
+	t_list	*env_lst;
+	t_list	*new;
+	t_env	*env;
 	int i;
 
-	env_lst = ft_calloc(sizeof(t_env) * 1);
-	if (!env_lst)
-		return (NULL);
 	i = 0;
-	head = env_lst;
+	env_lst = NULL;
 	while (envp[i])
 	{
-		env_lst->index = i;
-		env_lst->name = envp[i];
-		if (envp[i + 1])
-		{
-			env_lst->next = ft_calloc(sizeof(t_env) * 1);
-			if (!env_lst->next)
-				return (NULL);
-			env_lst->next->prev = env_lst;
-			env_lst = env_lst->next;
-		}
+		env = ft_calloc(sizeof(t_env) * 1);
+		if (!env)
+			return (NULL);
+		env->name = envp[i];
+
+		new = ft_lstnew(env);
+		if (!new)
+			return (NULL);
+		new->index = i;
+
+		ft_lstadd_back(&env_lst, new);
 		i++;
 	}
-	env_lst = head;
-	return(head);
+	return(env_lst);
 }
-
-// int	main(int argc, char **argv, char **envp)
-// {
-// 	t_env	*env_lst;
-// 	(void)argc;
-// 	(void)argv;
-
-// 	env_lst = ft_env_to_lst(envp);
-// 	// if (!env_lst->next) //Ajouter la gestion d'erreur !!
-
-// 	while (env_lst)
-// 	{
-// 		printf("%s\n", env_lst->name);
-// 		env_lst = env_lst->next;
-// 	}
-// }
 
 int	main(int argc, char **argv, char **envp)
 {
-	t_env	*env_lst;
-	t_env	*head;
-	int i = 0;
+	t_list	*env_lst;
 	(void)argc;
 	(void)argv;
-	env_lst = ft_calloc(sizeof(t_env) * 1);
-	
-	head = env_lst;
-	while (envp[i])
-		printf("%s\n", envp[i++]);
-	env_lst = head;
-	return(0);
+
+	env_lst = ft_env_to_lst(envp);
+	if (!env_lst) 
+		return (printf("Erreur : liste vide ou allocation échouée\n"), 1);
+	while (env_lst)
+	{
+		printf("%s\n", ((t_env *)env_lst->content)->name);
+		env_lst = env_lst->next;
+	}
+	return (0);
 }
+
