094ca862a63bae2d6469bcb1442aa225021564a4
mavander <mavander@student.42lehavre.fr>
2025-05-23 18:43:44 +0200

    LibFT Updated - Added ft_rand_str.c and its function

---
+++ lib/libft/custom/ft_rand_str.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rand_str.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mavander <mavander@student.42lehavre.fr>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/21 21:42:42 by mavander          #+#    #+#             */
+/*   Updated: 2024/12/21 21:42:42 by mavander         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+// Generates a random string of uppercase letters (A-Z) with specified length.
+// Uses /dev/urandom to get random bytes, mapping each byte to a letter.
+// Returns a malloc'd string that must be freed by the caller when done.
+char	*ft_rand_str(size_t len)
+{
+	int		fd;
+	unsigned char	*str;
+	size_t	i;
+
+	fd = open("/dev/urandom", O_RDONLY);
+	str = ft_calloc(len + 1);
+	if (!str)
+		return (NULL);
+	if (fd >= 0)
+		read(fd, str, len);
+	close(fd);
+	str[len] = '\0';
+	while (len--)
+		str[len] = 'A' + (str[len] % 26);
+	return ((char *)str);
+}
