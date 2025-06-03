e3dbf24de4ff91bcb32561ef95bd9ceac7fb820c
mavander <mavander@student.42lehavre.fr>
2025-06-03 15:13:27 +0200

    Cleared skibidi_shell.c

--- src/skibidi_shell.c
+++ src/skibidi_shell.c
@@ -11,28 +11,3 @@
 /* ************************************************************************** */
 
 #include "skibidi_shell.h"
-
-
-
-int	main(int ac, char **av, char **env)
-{
-	t_list	*cmd;
-	t_cmd	*content;
-
-	cmd = (t_list *)ft_calloc(sizeof(t_list));
-
-	cmd->content = ft_calloc(sizeof(t_cmd));
-
-	((t_cmd *)cmd->content)->redir = ft_calloc(sizeof(t_list));
-	((t_cmd *)cmd->content)->arg = ft_calloc(sizeof(t_list));
-
-	((t_cmd *)cmd->content)->redir->content = ft_calloc(sizeof(t_redir));
-	((t_cmd *)cmd->content)->arg->content = ft_calloc(sizeof(t_arg));
-
-	((t_redir *)((t_cmd *)cmd->content)->redir->content)->name = ft_calloc(sizeof(char));
-	((t_redir *)((t_cmd *)cmd->content)->redir->content)->name = ft_calloc(sizeof(char));
-	ft_lstadd_back(cmd, ft_lstnew(content));
-
-
-	((t_redir *)((t_cmd *)cmd->content)->redir->content)->type = OUTFILE;
-}
