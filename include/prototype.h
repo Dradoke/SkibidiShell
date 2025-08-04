fb3c16e9aaf47e18d8b77addd328a09c3955a912
Nathan Gaudouin <ngaudoui@c2r11p3.42lehavre.fr>
2025-08-04 20:48:15 +0200

    normed

--- include/prototype.h
+++ include/prototype.h
@@ -21,19 +21,19 @@
 /*████████████████████████████████████████████████████████████████████████████*/
 /*---debug---*/
 // print_list.c
-void				print_list(t_list *lst, t_lstype type);
+void			print_list(t_list *lst, t_lstype type);
 /*████████████████████████████████████████████████████████████████████████████*/
 /*---exec---*/
 // ft_heredoc.c
-t_bool				ft_heredoc(t_list *cmd);
+t_bool			ft_heredoc(t_list *cmd);
 /*████████████████████████████████████████████████████████████████████████████*/
 /*---parser---*/
 // ft_addarg.c
-t_bool				ft_addarg(t_shell *sh, t_cmd *tcmd);
+t_bool			ft_addarg(t_shell *sh, t_cmd *tcmd);
 // ft_addredir.c
-t_bool				ft_addredir(t_shell *sh, t_cmd *tcmd);
+t_bool			ft_addredir(t_shell *sh, t_cmd *tcmd);
 // ft_cpyword.c
-char				*ft_cpyword(t_shell *sh);
+char			*ft_cpyword(t_shell *sh);
 // ft_env_to_lst.c
 size_t			ft_get_env_size(t_shell *sh, char *src, size_t *i);
 char			*get_env_key(char *line);
@@ -41,7 +41,7 @@ char			*get_env_value(char *line);
 t_bool			ft_write_env(t_shell *sh, char *src, char *dst, size_t *i);
 t_list			*ft_env_to_lst(t_shell *sh, char **envp);
 // ft_parser.c
-t_bool				ft_parser(t_shell *sh);
+t_bool			ft_parser(t_shell *sh);
 /*████████████████████████████████████████████████████████████████████████████*/
 /*---utils---*/
 // ft_error.c
@@ -55,19 +55,19 @@ void			ft_seterror(t_shell *sh, char *msg, int code);
 void			ft_clearerror(t_shell *sh);
 int				ft_puterror(t_shell *sh);
 // ft_free.c
-void				ft_free_targ(void *targ);
-void				ft_free_tredir(void *tredir);
-void				ft_free_tenv(void *tenv);
-void				ft_free_tcmd(void *tcmd);
+void			ft_free_targ(void *targ);
+void			ft_free_tredir(void *tredir);
+void			ft_free_tenv(void *tenv);
+void			ft_free_tcmd(void *tcmd);
 // ft_isdelim.c
-t_bool				ft_isdelim(int c);
+t_bool			ft_isdelim(int c);
 // ft_lsttoarray.c
-char				**ft_argtoarray(t_list *list);
-char				**ft_envtoarray(t_list *list);
+char			**ft_argtoarray(t_list *list);
+char			**ft_envtoarray(t_list *list);
 // ft_signal.c
-void				sigint_handler(int sig);
+void			sigint_handler(int sig);
 // ft_skipspace.c
-t_bool				ft_skipspace(char *str, size_t *i);
+t_bool			ft_skipspace(char *str, size_t *i);
 // ft_var_expand.c
 size_t			ft_get_env_size(t_shell *sh, char *src, size_t *i);
 t_bool			ft_write_env(t_shell *sh, char *src, char *dst, size_t *i);
